0444d4b064c1641d9409152261ea669f4deaa821
Rohan Rustagi <110477025+RohanRusta21@users.noreply.github.com>
2023-03-25 15:45:50 +0000

    Modified Node_Modules

--- manage.py
+++ manage.py
@@ -1,5 +1,12 @@
 import os
 import sys
+import django
+from django.utils.encoding import force_str
+django.utils.encoding.force_text = force_str
+from six import python_2_unicode_compatible
+#from six import python_2_unicode_compatible
+from django.utils.encoding import smart_str
+django.utils.encoding.smart_text = smart_str
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "home.settings.dev")
